1ac3693435088015db43d4a20e964a8401a76bea
Courtney Dart <dart.courtney1@gmail.com>
2020-11-03 10:59:58 -0800

    Create a dicionary from two arrays of the same length.

---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/DictionaryOfArrays/MakeDictionaryProblem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryOfArrays
+{
+    class MakeDictionaryProblem
+    {
+        public static Dictionary<string, int> MakeDict(string[] strArray, int[] numArray)
+        {
+            var arrayDict = new Dictionary<string, int>();
+            int j = 0;
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (j == i)
+                {
+                    arrayDict.Add(strArray[i], numArray[j]);
+                    j++;
+                }
+            }
+            return arrayDict;
+        }
+        public static Dictionary<string, int> MakeDict2(string[] strArray, int[] numArray)
+        {
+            // With the same length, we can use 'i' to iterate through both arrays.
+            var arrayDict = new Dictionary<string, int>();
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                arrayDict.Add(strArray[i], numArray[i]);
+            }
+            return arrayDict;
+        }
+    }
+}
---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/DictionaryOfArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryOfArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strArray = { "a", "b", "c", "d" };
+            int[] numArray = { 1, 2, 3, 4 };
+
+            var dict = MakeDictionaryProblem.MakeDict(strArray, numArray);
+            foreach (KeyValuePair<string, int> kvp in dict)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            }
+            Console.WriteLine("=======================");
+            var dict2 = MakeDictionaryProblem.MakeDict2(strArray, numArray);
+            foreach (KeyValuePair<string, int> kvp in dict2)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
