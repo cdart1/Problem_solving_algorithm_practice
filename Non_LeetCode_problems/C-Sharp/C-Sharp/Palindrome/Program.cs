ef820eec4a0efd0ad0ee8c7b3af05ffec7c17b9e
Courtney Dart <dart.courtney1@gmail.com>
2020-11-03 20:25:48 -0800

    Checked for a string palindrome using StringBuilders, for loops, bool values, bool variables, as well as manually converted char array to string.

---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/Palindrome/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace StringPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, return true if the string is a palindrome and false if it isn’t.
+
+            string str = "racecar";
+            string str2 = "jump rope";
+
+            //int[] num = { 1, 2, 3, 4 };
+            //int[] num2 = { 1, 2, 3, 4 };
+
+            //if (num.ToString() == num2.ToString())
+            //{
+            //    Console.WriteLine("true");
+            //}
+            //Console.WriteLine("==================");
+
+
+
+
+            bool answer = StringPalindromeProblem.IsPalindrome(str);
+            Console.WriteLine(answer);
+            Console.WriteLine("==================");
+            bool answer2 = StringPalindromeProblem.IsPalindrome2(str);
+            Console.WriteLine(answer2);
+            Console.WriteLine("==================");
+            bool answer3 = StringPalindromeProblem.IsPalindrome3(str);
+            Console.WriteLine(answer3);
+            Console.WriteLine("==================");
+
+
+            /*
+            // Can convert strings to char arrays easily. It's hard to convert char arrays to strings.
+            StringBuilder sb = new StringBuilder();
+            sb.Append("nicson");
+
+            // string object (static)
+            char[] name = "nicson".ToCharArray();
+            // StringBuilder object (dynamic)
+            char[] name2 = sb.ToString().ToCharArray();
+            foreach (char c in name2)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("==================");
+
+            // This logic doesn't work.
+            string name3 = name.ToString();
+            Console.WriteLine(name3);
+            Console.WriteLine("==================");
+            */
+        }
+    }
+}
---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/Palindrome/StringPalindromeProblem.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringPalindrome
+{
+    class StringPalindromeProblem
+    {
+        /// <summary>
+        /// Checking for a palindrome using two StringBuilders and two for loops (hard coded boolean values).
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>
+        /// Returns true if it's a palindrome, returns false if it's not.
+        /// </returns>
+        public static bool IsPalindrome(string str)
+        {
+            // If you want to convert a collection of characters to strings, use StringBuilder.
+            StringBuilder charList1 = new StringBuilder();
+            StringBuilder charList2 = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                charList1.Append(str[i]);
+                Console.WriteLine(str[i]);
+            }
+            // str.Length - 1 gives the last index of the string.
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                charList2.Append(str[i]);
+                Console.WriteLine(str[i]);
+            }
+            // Can't compare collections eg. Lists, Arrays, Dictionaries, StringBuilder. Can only compare primative data types eg. int, string, char, bool.
+            // That is why we use ToString().
+            if (charList1.ToString() == charList2.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checking for a palindrome using two StringBuilders and two for loops (using a bool variable).
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>
+        /// Returns true if it's a palindrome, returns false if it's not.
+        /// </returns>
+        public static bool IsPalindrome2(string str)
+        {
+            StringBuilder charList1 = new StringBuilder();
+            StringBuilder charList2 = new StringBuilder();
+            bool isPalindrome = false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                charList1.Append(str[i]);
+            }
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                charList2.Append(str[i]);
+            }
+            if (charList1.ToString() == charList2.ToString())
+            {
+                isPalindrome = true;
+            }
+            return isPalindrome;
+        }
+
+        /// <summary>
+        /// Checking for a palindrome using one StringBuilder, one for loop and compares it with the input string (using a bool variable).
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>
+        /// Returns true if it's a palindrome, returns false if it's not.
+        /// </returns>
+        public static bool IsPalindrome3(string str)
+        {
+            StringBuilder charList = new StringBuilder();
+            bool isPalindrome = false;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                charList.Append(str[i]);
+            }
+            Console.WriteLine(str + " " + charList.ToString());
+            if (charList.ToString() == str)
+            {
+                isPalindrome = true;
+            }
+            return isPalindrome;
+        }
+
+        /// <summary>
+        /// Checking for a palindrome using one character array, one for loop and compares it with the input string, and manually converting 
+        /// the char array to a string (using a bool variable).
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>
+        /// Returns true if it's a palindrome, returns false if it's not.
+        /// </returns>
+        public static bool IsPalindrome4(string str)
+        {
+            char[] charList2 = new char[str.Length];
+            bool isPalindrome = false;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                charList2[i] = str[i];
+            }
+            
+            // I'm converting charList1 and charList2 to strings.
+            string string2 = new string(charList2);
+
+            if (str == string2)
+            {
+                isPalindrome = true;
+            }
+            return isPalindrome;
+        }
+    }
+}
