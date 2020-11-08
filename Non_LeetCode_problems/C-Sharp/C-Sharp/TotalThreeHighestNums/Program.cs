daa18cc770fdf7b6cedc41eaaa0f6f8d224c0325
Courtney Dart <dart.courtney1@gmail.com>
2020-11-07 23:14:29 -0800

    Found sum of three highest numbers in given array usign bubble sort and lists. Learned about time complexity O(n) and O(n)^2.

---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/TotalThreeHighestNums/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TotalThreeHighestNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 1, 2, 3, 4, 5, 6 };
+            int sum = Add3HighestNumsProblem.Add3Highest(inputArray);
+            Console.WriteLine(sum);
+        }
+    }
+}
