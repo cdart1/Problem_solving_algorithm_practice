78079f1ee2426c09dd245ac14536561647d88673
Courtney Dart <dart.courtney1@gmail.com>
2020-11-12 21:09:37 -0800

    Added new coding projects.

---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/NumberOfStepsToZero/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberOfStepsToZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 14;
+            int answer = StepsToZeroProblem.ReduceToZero2(num);
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/NumberOfStepsToZero/StepsToZeroProblem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberOfStepsToZero
+{
+    class StepsToZeroProblem
+    {
+        public static int ReduceToZero(int num)
+        {
+            int count = 0;
+            while(num > 0)
+            {
+                if(num % 2 == 0)
+                {
+                    num = num / 2;
+                    count++;
+                }
+                else
+                {
+                    num = num - 1;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int ReduceToZero2(int num)
+        {
+            int count = 0;
+            Recursion(num, count);
+            return count;
+        }
+
+        public static int Recursion(int num, int count)
+        {
+            if (num > 0 && num % 2 == 0)
+            {
+                num = num / 2;
+                return Recursion(num, count);
+            }
+            else if (num > 0 && num % 2 != 0)
+            {
+                num = num - 1;
+                return Recursion(num, count);
+            }
+            return;
+        }
+    }
+}
