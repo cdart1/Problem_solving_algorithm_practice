fcb97b3a3af3812f9c35db5973d354258dfbb855
Courtney Dart <dart.courtney1@gmail.com>
2020-11-16 21:48:53 -0800

    Added new algorithm problems.

---
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/ToLowerCase/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ToLowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "HeLlO P3#";
+            string answer = ToLowerCaseProblem.ToLowerCase2(s);
+            Console.WriteLine(answer);
+        }
+    }
+}
