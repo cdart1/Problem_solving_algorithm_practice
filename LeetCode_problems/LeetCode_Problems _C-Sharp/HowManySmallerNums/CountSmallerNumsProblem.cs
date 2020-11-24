79df6bcd85a207b42a2cf505be0252a080b6674a
Courtney Dart <dart.courtney1@gmail.com>
2020-11-23 18:28:23 -0800

    Added new LeetCode problem.

---
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/HowManySmallerNums/CountSmallerNumsProblem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HowManySmallerNums
+{
+    class CountSmallerNumsProblem
+    {
+        public static List<int> CountSmallerNums(int[] nums)
+        {
+            int count = 0;
+            List<int> countArray = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        count++;
+                    }
+                }
+                countArray.Add(count);
+                count = 0;
+            }
+            return countArray;
+        }
+
+        public static int[] CountSmallerNums2(int[] nums)
+        {
+            int count = 0;
+            int[] countArray = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        count++;
+                    }
+                }
+                countArray[i] = count;
+                count = 0;
+            }
+            return countArray;
+        }
+    }
+}
---
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/HowManySmallerNums/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HowManySmallerNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 8, 1, 2, 2, 3 };
+            List<int> answers = CountSmallerNumsProblem.CountSmallerNums(nums);
+            foreach (var answer in answers)
+            {
+                Console.WriteLine(answer);
+            }
+            Console.WriteLine("================");
+            int[] answers2 = CountSmallerNumsProblem.CountSmallerNums2(nums);
+            foreach (var answer in answers2)
+            {
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
