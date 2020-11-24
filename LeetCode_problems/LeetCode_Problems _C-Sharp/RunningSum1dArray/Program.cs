b04f66a8df86e152c029dde99ddb3a42ecd66811
Courtney Dart <dart.courtney1@gmail.com>
2020-11-23 21:46:39 -0800

    Added RunningSum Problem.

---
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/RunningSum1dArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RunningSum1dArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/RunningSum1dArray/RunningSumProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RunningSum1dArray
+{
+    public class RunningSumProblem
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            return new int[nums.Length];
+        }
+    }
+}
