d009347669f2194122f529e0822904dd79be36a7
Courtney Dart <dart.courtney1@gmail.com>
2020-12-28 15:42:03 -0800

    Learned why we need 'head.next==null' in code by running test. Noted this as a comment.

--- LeetCode_problems/LeetCode_Problems _C-Sharp/ReverseLinkedList/Program.cs
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/ReverseLinkedList/Program.cs
@@ -18,6 +18,8 @@ namespace ReverseLinkedList
             int secondNode = node.next.val;
             int thirdNode = node.next.next.val;
             int fourthNode = node.next.next.next.val;
+            // This variable will give a null reference error which is why we made 'head.next == null' in our code.
+            // int fourthNode2 = node.next.next.next.next.val;
 
             Console.WriteLine("{0} {1} {2} {3}", firstNode, secondNode, thirdNode, fourthNode);
         }
