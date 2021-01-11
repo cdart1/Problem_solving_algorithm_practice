f4c8b5f27d869e49ccbd0cf1f10cdd4915abdda2
Courtney Dart <dart.courtney1@gmail.com>
2021-01-11 12:58:33 -0800

    Implemented Binary Tree class, searched the tree in Post Order returning a string.

---
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/BinaryTreeImplementation/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeImplementation
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node (int val, Node left = null, Node right = null)
+        {
+            Value = val;
+            Left = left;
+            Right = right;
+        }
+    }
+}
--- Non_LeetCode_problems/C-Sharp/C-Sharp/BinaryTreeImplementation/Program.cs
+++ Non_LeetCode_problems/C-Sharp/C-Sharp/BinaryTreeImplementation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BinaryTreeImplementation
 {
@@ -6,7 +7,33 @@ namespace BinaryTreeImplementation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node root = new Node(1, 
+                new Node(2, 
+                    new Node(4), 
+                    new Node(5)
+                    ), 
+                new Node(3, 
+                    new Node(6), 
+                    new Node(7))
+                );
+
+            Console.WriteLine(BTreeString(root));
+
+            static string BTreeString(Node root)
+            {
+                string nodeStr = "";
+
+                if (root.Left != null)
+                {
+                    nodeStr += BTreeString(root.Left);
+                }
+                if (root.Right != null)
+                {
+                    nodeStr += BTreeString(root.Right);
+                }
+                
+                return nodeStr += root.Value;
+            }
         }
     }
 }
