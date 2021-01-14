ce178e9f4550eee834c280bdc06d4e468ff76432
Courtney Dart <dart.courtney1@gmail.com>
2021-01-14 13:38:01 -0800

    Solved longest word algorithm with different approaches.

---
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/longestWordInDictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace longestWordInDictionary
+{
+    class Program
+    {
+        /* Given a list of strings words representing an English Dictionary, find the longest word 
+         * in words that can be built one character at a time by other words in words.If there is 
+         * more than one possible answer, return the longest word with the smallest lexicographical order.
+
+            If there is no answer, return the empty string.
+            Example 1:
+            Input: 
+            words = ["w", "wo", "wor", "worl", "world"]
+            Output: "world"
+            Explanation: 
+            The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+
+            Example 2:
+            Input: 
+            words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+            Output: "apple"
+            Explanation: 
+            Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is 
+            lexicographically smaller than "apply".
+
+            Note:
+            All the strings in the input will only contain lowercase letters.
+            The length of words will be in the range [1, 1000].
+            The length of words[i] will be in the range [1, 30].
+        */
+
+        static void Main(string[] args)
+        {
+            string[] words = new string[] { "d", "do", "dog", "p", "pe", "pen", "peng", "pengu", "pengui", "penguin", "e", "el", "ele", "elep", "eleph", "elepha", "elephan", "elephant" };
+            string[] words2 = new string[] { "a", "banana", "app", "appl", "ap", "apply", "apple" };
+            string[] words3 = new string[] { "w", "wo", "wor", "worl", "world" };
+            string answer = LongestWordProblem.LongestWord(words);
+            string answer2 = LongestWordProblem.LongestWord(words2);
+            string answer3 = LongestWordProblem.LongestWord(words3);
+            Console.WriteLine(answer);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
+            Console.WriteLine("=======================");
+            string answer4 = LongestWordProblem.LongestWord2(words);
+            string answer5 = LongestWordProblem.LongestWord2(words2);
+            string answer6 = LongestWordProblem.LongestWord2(words3);
+            Console.WriteLine(answer4);
+            Console.WriteLine(answer5);
+            Console.WriteLine(answer6);
+        }
+    }
+}
---
+++ LeetCode_problems/LeetCode_Problems _C-Sharp/longestWordInDictionary/longestWordProblem.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace longestWordInDictionary
+{
+    public class LongestWordProblem
+    {
+        /// <summary>
+        /// This method takes in an array of strings and gets the largest, lexicographically smallest string. 
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns>This method return the longest string.</returns>
+        public static string LongestWord(string[] words)
+        {
+            string ans = "";
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                set.Add(words[i]);
+            }
+            for (int j = 0; j < words.Length; j++)
+            {
+                if (words[j].Length > ans.Length ||
+                    // If the current lengths are equal and current string preceeds the 'ans'
+                    words[j].Length == ans.Length && words[j].CompareTo(ans) < 0)
+                {
+                    bool isGood = true;
+                    // 'k' starts at '1'. 'k' determines the number of characters in the substring.
+                    for (int k = 1; k < words[j].Length; k++)
+                    {
+                        // If the set does contain the specified substring (this condition isn't hit)
+                        if (!set.Contains(words[j].Substring(0, k)))
+                        {
+                            isGood = false;
+                            break;
+                        }
+                    }
+                    if (isGood)
+                    {
+                        ans = words[j];
+                    }
+                }
+            }
+            return ans;
+        }
+
+
+        /// <summary>
+        /// This method takes an array and gets the lexicographically smallest 'answer' based on the length of 'goodList'. 
+        /// <param name="words"></param>
+        /// <returns>This method return the longest string.</returns>
+        public static string LongestWord2(string[] words)
+        {
+            string answer = "";
+            List<string> goodList = new List<string>();
+            List<int> indexListSameLen = new List<int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                // goodList adds a string candidate after comparing lengths of the current string, 'word[i]' and the last 
+                // string added in 'goodList'. 
+                bool isCandidate = IsCandidate(words[i], words);
+                if (isCandidate)
+                {
+                    if (goodList.Count == 0)
+                    {
+                        goodList.Add(words[i]);
+                    }
+                    if (goodList[goodList.Count - 1].Length < words[i].Length)
+                    {
+                        // This list's length, 'indexListSameLen' determines whether the last string in goodList gets replaces,
+                        // or all strings get replaced with the current string, words[i].
+                        if (indexListSameLen.Count <= 1)
+                        {
+                            goodList.RemoveAt(goodList.Count - 1);
+                            goodList.Add(words[i]);
+                        }
+                        if (indexListSameLen.Count > 1)
+                        {
+                            goodList.Clear();
+                            goodList.Add(words[i]);
+                            indexListSameLen.Clear();
+                        }
+                    }
+                    // If both string lengths are same or the the last string in gooList is larger, 
+                    if (goodList[goodList.Count - 1].Length >= words[i].Length)
+                    {
+                        // make sure they the strings aren't identical and the last string in goodList is smaller or equal in length.
+                        if (goodList[goodList.Count - 1] != words[i] && goodList[goodList.Count - 1].Length <= words[i].Length)
+                        {
+                            // If strings are equal lengths, add the current string to goodList and add the indexes
+                            // in which the current string and the last string in goodlist are positioned.
+                            if (goodList[goodList.Count - 1].Length == words[i].Length)
+                            {
+                                goodList.Add(words[i]);
+                                indexListSameLen.Add(goodList.Count - 1);
+                                indexListSameLen.Add(goodList.Count - 2);
+                            }
+                        }
+
+                    }
+                }
+            }
+            if (goodList.Count == 1)
+            {
+                answer = goodList[0];
+            }
+            if (goodList.Count > 1)
+            {
+                answer = LexicographicallySmallest(goodList);
+            }
+            return answer;
+        }
+
+        /// <summary>
+        ///  This method takes an array and a string. It checks if a hashset contains a word and adds to the count.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="words"></param>
+        /// <returns>This method returns a bool.</returns>
+        public static bool IsCandidate(string word, string[] words)
+        {
+            bool isCandidate = false;
+            int count = 0;
+            string makingWord = "";
+            HashSet<string> prefixList = new HashSet<string>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                makingWord += word[i].ToString();
+                prefixList.Add(makingWord);
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (prefixList.Contains(words[i]))
+                {
+                    count++;
+                }
+                if (count == prefixList.Count)
+                {
+                    isCandidate = true;
+                }
+            }
+            return isCandidate;
+        }
+        /// <summary>
+        /// This method iterates through a list, compares lengths of two strings to get the smaller one.
+        /// </summary>
+        /// <param name="goodListStr"></param>
+        /// <returns>This method returns the smallest string.</returns>
+        //Find the smallest of the two strings after getting their ASCII values.
+        public static string LexicographicallySmallest(List<string> goodListStr)
+        {
+            string smallest = "";
+            for (int i = 0; i < goodListStr.Count-1; i++)
+            {
+                string str1 = goodListStr[i];
+                string str2 = goodListStr[i + 1];
+                for (int j = 0; j < str1.Length; j++)
+                {
+                    if (str1[j] != str2[j])
+                    {
+                        if (str1[j] > str2[j])
+                        {
+                            smallest = str2;
+                            return smallest;
+                        }
+                        if (str2[j] > str1[j])
+                        {
+                            smallest = str1;
+                            return smallest;
+                        }
+                    }
+                }
+            }
+            return smallest;
+        }
+    }
+}
